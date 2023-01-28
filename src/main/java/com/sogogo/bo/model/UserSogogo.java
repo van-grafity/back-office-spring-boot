577462b8a24402b693f4779de7e9f2a66885867f
noctrunalcoders <christiantolie99@gmail.com>
2023-01-28 18:31:21 +0700

    change configuration

--- src/main/java/com/sogogo/bo/model/UserSogogo.java
+++ src/main/java/com/sogogo/bo/model/UserSogogo.java
@@ -1,16 +1,20 @@
 package com.sogogo.bo.model;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "user_sogogo")
 public class UserSogogo {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", columnDefinition = "uuid", length = 36, nullable = false)
+    private UUID id;
 
     private String name;
     private String username;
